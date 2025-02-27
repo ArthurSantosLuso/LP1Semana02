f4e88c5a4f48d9db8f0f0251d4aa1bceca69dd4d
Arthur Santos <arthurluiz.developer@gmail.com>
2025-02-27 09:44:40 +0000

    Exercicio 5 Iniciado

---
+++ Cilindro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Altura do cilindro
+            double a = double.Parse(Console.ReadLine());
+
+            // Raio do cilindro
+            double r = double.Parse(Console.ReadLine());
+
+            // Calc do volume
+            double V = Math.PI * Math.Pow(r,2) * a;
+
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "{0:F3}", V));
+
+        }
+    }
+}
