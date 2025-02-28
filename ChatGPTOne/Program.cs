3778e7c091bdcfe97f8c2f2e32c718a9ba573e7f
Arthur Santos <arthurluiz.developer@gmail.com>
2025-02-28 13:37:39 +0000

    Perguntas possiveis de serem respondidas adiciondas ao projeto.

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ChatGPTOne
 {
@@ -8,9 +9,31 @@ namespace ChatGPTOne
         {
             while (true)
             {
+                string resposta = "";
+
                 // Pedir uma pergunta ao utilizador 
                 Console.Write("Insira uma pergunta: ");
                 string pergunta = Console.ReadLine();
+
+                // Casos de respostas a perguntas 
+                switch (pergunta)
+                {
+                    case "Quantos anos tens?":
+                        resposta = "O suficiente para lhe responder todas as perguntas.";
+                        break;
+
+                    case "Qual a sensação de ser uma IA":
+                        resposta = "A mesmsa que ser uma formiga, eu não sei.";
+                        break;
+
+                    case "Qual a melhor forma de fazer dinheiro?":
+                        resposta = "Entre na politica e pege fundão eleitoral.";
+                        break;
+
+                    case "Porque o mundo é uma bola?":
+                        resposta = "A terra é plana.";
+                        break;
+                }
             }
         }
     }
