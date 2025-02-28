d7efd5ab3e875b3916421260956f9a27f646e919
Arthur Santos <arthurluiz.developer@gmail.com>
2025-02-28 13:31:41 +0000

    Exercicio 10 iniciado, o programa ja pede uma pergunta ao chat bot ao utilizador

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                // Pedir uma pergunta ao utilizador 
+                Console.Write("Insira uma pergunta: ");
+                string pergunta = Console.ReadLine();
+            }
+        }
+    }
+}
